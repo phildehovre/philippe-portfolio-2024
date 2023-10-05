95a60128a0299f9ae9bcae6246014e8306b58b9e
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-10-05 11:27:40 +0100

    backgrop scale fixed - LG

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,11 +1,28 @@
+import { useLayoutEffect } from "react";
 import "./Backdrop.scss";
+import gsap from "gsap";
 
 function Backdrop() {
-  return (
-    <div className="backdrop">
-      <div className="gradient"></div>
-    </div>
-  );
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.set(".backdrop", { scale: 0.6 });
+      gsap
+        .to(
+          ".backdrop",
+
+          {
+            scale: 1,
+            duration: 7,
+            y: "25%",
+          }
+        )
+        .repeat(-1)
+        .yoyo(true);
+    });
+    return () => ctx.revert();
+  }, []);
+
+  return <div className="backdrop"></div>;
 }
 
 export default Backdrop;
