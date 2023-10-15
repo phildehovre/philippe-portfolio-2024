4170788acf2a69aed65ef4fc9df6e3a3a1713409
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-10-15 15:49:45 +0100

    about me text

--- src/lib/text.ts
+++ src/lib/text.ts
@@ -285,5 +285,5 @@ export const aboutMe = {title: 'About me', content: `As a musician turned develo
 
 My multilingual skills, including native proficiency in French and English, are assets that enable effective communication in diverse teams.
 \n
-I'm excited about opportunities to contribute my skills and enthusiasm to innovative organizations, such as Itransition, Civica, and Acurable, and I look forward to discussing how I can make a meaningful impact on your team.`
+I'm excited about opportunities to contribute my skills and enthusiasm to innovative organizations, I look forward to discussing how I can make a meaningful impact on your team.`
 }
